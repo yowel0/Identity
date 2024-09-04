18675c9520cf348ad83ca3bd2a6858e3834d9fcf
Haelinos <hznieland@outlook.com>
2024-09-04 22:56:04 +0200

    Script is back yippiee

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    public AudioClip background;
+    public AudioClip SFX;
+
+    private void Start()
     {
-        
+        musicSource.clip = background;
+        musicSource.Play();
+
+        musicSource.clip = SFX;
+        musicSource.Play();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void PlaySFX(AudioClip clip) 
+    { 
+        SFXSource.PlayOneShot(clip);
+
     }
 }
+
